db1b3f82eca5b36632ead734bbecfc8c0d6472ac
boaz blake <boazblake@gmail.com>
2017-09-05 11:32:00 -0500

    updated db

--- config/db-setup.js
+++ config/db-setup.js
@@ -5,29 +5,26 @@ module.exports = {
    connectToDB: function(projectName){
       console.log('running db-setup')
       let dbLocation = 'mongodb://admin:romedogBB79!@ds115214.mlab.com:15214/tenant_fit'
+      //'mongodb://localhost/'+ projectName
 
       if (process.env.NODE_ENV === "development"){
-        //dbLocation += "_dev"
-        mongoose.connect(dbLocation ,
-          { socketTimeoutMS: 0
-          , keepAlive: true
-          , reconnectTries: 30
-          } ,(err, db)=>{
-              if (err) {
-                console.log(err)
-              }
-              else console.log("\n\n===== Connected to: " + dbLocation +  "=====\n\n")
+        dbLocation += "_dev"
+        mongoose.connect(dbLocation , (err, db)=>{
+          if (err) {
+            console.log(err)
+          }
+          else {
+            console.log("\n\n===== Connected to: " + dbLocation +  "=====\n\n")
+          }
         })
       } else {
-        mongoose.connect(dbLocation ,
-          { socketTimeoutMS: 0
-          , keepAlive: true
-          , reconnectTries: 30
-          } , (err, db) => {
-            if (err) {
-              console.log(err)
-            }
-            else console.log("\n\n===== Connected to: " + dbLocation +  "=====\n\n")
+        mongoose.connect(process.env.MONGODB_URI , (err, db)=>{
+          if (err) {
+            console.log(err)
+          }
+          else {
+            console.log("\n\n===== Connected to: " + dbLocation +  "=====\n\n")
+          }
         })
       }
       console.log('finished setup')
