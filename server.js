9e4d49ec7104c236f8f5e99cb67d199385cfdcf4
boaz blake <boazblake@gmail.com>
2017-09-09 10:00:23 -0500

    updated logic to serve

--- server.js
+++ server.js
@@ -58,6 +58,10 @@ app.use( appMiddleWare.parseQuery )
 // ROUTERS
 // =========
 app.use(function(req, res, next) {
+  if (req.header.Host === "https://tenant-fit.herokuapp.com") {
+    req.header.Host = "http://localhost:9000"
+  }
+
   res.header("Access-Control-Allow-Origin",["http://localhost:9000"]);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
